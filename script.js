a8365bb602f22ca23a56076e891d9436bc598ec6
prasetion <prasetion@gmail.com>
2024-08-11 11:45:22 +0700

    modify getdelta to getelapsedtime  in script.js

--- script.js
+++ script.js
@@ -190,13 +190,19 @@ const clock = new THREE.Clock();
 let previousTime = 0;
 
 const tick = () => {
-  const elapsedTime = clock.getDelta();
+  const elapsedTime = clock.getElapsedTime();
   const deltaTime = elapsedTime - previousTime;
   previousTime = elapsedTime;
+
+  for (const mesh of sectionMesh) {
+    mesh.rotation.x += deltaTime * 0.1;
+    mesh.rotation.y += deltaTime * 0.12;
+  }
 
   // Animate camera
   camera.position.y = (-scrollY / sizes.height) * objectsDistance;
 
+  // parallax
   const parallaxX = cursor.x * 0.5;
   const parallaxY = -cursor.y * 0.5;
   cameraGroup.position.x +=
@@ -204,11 +210,6 @@ const tick = () => {
   cameraGroup.position.y +=
     (parallaxY - cameraGroup.position.y) * 5 * deltaTime;
 
-  // Animate meshes
-  for (const mesh of sectionMesh) {
-    mesh.rotation.x += deltaTime * 0.1;
-    mesh.rotation.y += deltaTime * 0.12;
-  }
   // Render
   renderer.render(scene, camera);
 
